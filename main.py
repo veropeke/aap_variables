7767d009d74ae158d36e33de28acc1845ec3f68b
veropeke <verochera@hotmail.com>
2020-01-14 03:40:10 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#saludo = "hola "
+#nombre = input("ingrese su nombre: ")
+#print(saludo + nombre)
+
+
+num1 = int(input("ingrese un numero: "))
+num2 = int(input("ingrese segundo numero: "))
+
+print(num1 + num2)
